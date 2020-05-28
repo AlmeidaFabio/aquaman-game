606ebbae76e54149cb37f0c59e80feec718da3cc
AlmeidaFabioO <fabio.633@hotmail.com>
2020-05-28 18:32:11 -0300

    alterando styles

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
         </div>
 
 
-    <div class="container-fluid">
+        <div class="container-fluid">
         <div class="row">
             <div class="col" >
                 <div class="card middle">
